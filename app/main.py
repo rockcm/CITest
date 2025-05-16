b62dffcfeaa610d1894bdc0bca36fb74664fe919
Christian Rock <rockcm@etsu.edu>
2025-05-16 12:10:46 -0400

    added name

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI!"}
+
+@app.get("/name")
+def read_name():
+    return {"message": "Christian"}
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,9 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello from FastAPI!"}
+    
+def test_read_name():
+    response = client.get("/name")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Christian"}
+    
